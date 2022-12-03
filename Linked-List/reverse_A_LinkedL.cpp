064702530d2e93447b5c19b481e373bc46255b07
indicgenous <sid07102002@gmail.com>
2022-12-03 18:16:30 +0530

    Reverse a LL

---
+++ Linked-List/reverse_A_LinkedL.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+
+class node{
+    public:
+    int data;
+    node * next;
+
+    node(int val){
+        this->data=val;
+        this->next=NULL;
+    }
+};
+
+void insertathead(node * &head,int val){
+    node * newnode = new node(val);
+    newnode->next=head;
+    head=newnode;
+}
+void display(node * &head){
+    node * temp = head;
+    while ((temp!=NULL)){
+        cout<<temp->data<<" -> ";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+// ******************************************************************************************************************** \\ 
+
+//  reverse a liked list 
+
+node * reversell(node * &head){
+    if(head==NULL || head->next==NULL){
+        return head;
+    }
+    node * prev =NULL;
+    node * current =head;
+
+    while(current!=NULL){
+        node * forward = current->next;
+        current->next=prev;
+        prev=current;
+        current=forward;
+    }
+    head=prev;
+    return head;
+}
+
+// ******************************************************************************************************************** \\ 
+
+int main(){
+    node * newnode=new node(5);
+    node *head=newnode;
+    insertathead(head,4);
+    insertathead(head,3);
+    display(head);
+    cout<<endl;
+    reversell(head);
+    display(head);
+    cout<<head->data<<endl;
+}
