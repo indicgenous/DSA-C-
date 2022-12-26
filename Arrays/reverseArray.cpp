39ea17f0b179efecd44cf1e129bab9d4ccb92b9a
indicgenous <sid07102002@gmail.com>
2022-12-26 14:07:01 +0530

    arr

--- Arrays/reverseArray.cpp
+++ Arrays/reverseArray.cpp
@@ -2,6 +2,10 @@
 #include <stdio.h>
 using namespace std;
 
+
+
+//  with pointers
+
 void reverseArray(int arr[],int len){
     
     // cout<<len<<endl;
@@ -20,11 +24,31 @@ void reverseArray(int arr[],int len){
     for(int i =0;i<len;i++){
         cout<<arr[i]<<" ";
     }
+    cout<<endl;
+}
+
+// without pointers
+
+void arrayreverse(int arr[],int len){
+    int s=0;
+    int e=len-1;
+    while(s<e){
+        int temp =arr[e];
+        arr[e]=arr[s];
+        arr[s]=temp;
+        s++;
+        e--;
+    }
+    for(int i=0;i<len;i++){
+        cout<<arr[i]<<" ";
+    }
 }
 
 int main(){
     int arr[]={1,2,3,4,5,6};
+    int arr1[]={1,2,3,4,5,6};
     int len=sizeof(arr)/sizeof(int);
     reverseArray(arr,len);
+    arrayreverse(arr1,len);
 
 }
